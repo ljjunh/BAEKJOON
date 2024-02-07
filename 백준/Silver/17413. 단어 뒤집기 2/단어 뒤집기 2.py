fc730a47c18fc51ed4256abed956461f852dd2bb
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-02-08 00:39:54 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 1860 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,20 +1,20 @@
-# < : 나오면 전부다 출력append 뒤에 공백이 나와두 >가 없으면 출력 ㄴ
-# > : 정상출력
-from collections import deque
-string = input() + " "
-stack = deque([])
+string = input()+" "
+stack = []
+temp = False
 for i in string:
-    if i == " " and ">" not in stack and "<" not in stack:
+    if i == " " and temp == False:
         while stack:
             print(stack.pop(), end = "")
         print(" ", end = "")
-    elif i == ">":
-        stack.append(i)
-        while stack:
-            print(stack.popleft(), end = "")
     elif i == "<":
+        temp = True
         while stack:
             print(stack.pop(), end = "")
         stack.append(i)
+    elif i == ">":
+        stack.append(i)
+        while stack:
+            print(stack.pop(0), end = "")
+        temp = False
     else:
         stack.append(i)

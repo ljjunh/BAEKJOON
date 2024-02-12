0e52691307d75ee85c328fc78bac86e2120f4e0c
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-02-12 16:58:31 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 2464 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,16 +1,18 @@
 import sys
 input = sys.stdin.readline
-arr = [1] * 1000001
-for i in range(2, int(1000001 ** 0.5)+1):
+n = 1000001
+arr = [1] * n
+for i in range(2, int(n ** 0.5) + 1):
     if arr[i]:
-        for j in range(i * 2, 1000001, i):
+        for j in range(i * 2, n, i):
             arr[j] = 0
-
 T = int(input())
-for i in range(T):
+for _ in range(T):
+    num = int(input())
     cnt = 0
-    n = int(input())
-    for i in range(2, n//2 + 1):
-        if arr[i] and arr[n-i]:
+    for i in range(2, num//2 + 1):
+        if arr[i] and arr[num-i]:
             cnt += 1
     print(cnt)
+
+

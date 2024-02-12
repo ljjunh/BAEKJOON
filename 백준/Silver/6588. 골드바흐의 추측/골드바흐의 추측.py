8cf82142bdcd74b636ade16c2bf715f34cfe1f80
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-02-12 16:01:00 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 420 ms, Memory: 119484 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,19 +1,18 @@
 import sys
 input = sys.stdin.readline
-num = 1000001
-arr = [1] * num
-for i in range(3, int(num ** 0.5)+1, 2):
+n = 1000001
+arr = [1] * n
+for i in range(3, int(n ** 0.5) + 1, 2):
     if arr[i]:
-        for j in range(i*2, num, i):
+        for j in range(i * 2, n, i):
             arr[j] = 0
-
 while True:
-    n = int(input())
-    if n == 0:
+    k = int(input())
+    if k == 0:
         break
-    for i in range(3, n//2 + 1, 2):
-        if arr[i] and arr[n-i]:
-            print(f"{n} = {i} + {n-i}")
+    for i in range(3, int(k / 2) + 1, 2):
+        if arr[i] and arr[k - i]:
+            print(f"{k} = {i} + {k - i}")
             break
     else:
         print("Goldbach's conjecture is wrong.")

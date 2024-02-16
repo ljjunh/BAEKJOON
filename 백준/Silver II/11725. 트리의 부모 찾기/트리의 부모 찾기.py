391ffba31a1a26e64096e695a463518d1befec91
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-02-16 14:36:55 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 312 ms, Memory: 49348 KB -BaekjoonHub

--- 백준/Silver II/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver II/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,29 +1,27 @@
+from collections import deque
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-def dfs(idx, before):
-    global graph, visited, lst
-    visited[idx] = True
-    lst[idx] = before
-    for i in graph[idx]:
-        if not visited[i]:
-            dfs(i, idx)
+def dfs(idx):
+    global graph, visited
+    visited[idx] = 1
+    q = deque([idx])    
+    while q:
+        v = q.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = v
 
 
-
-# 0. 입력 및 초기화
 N = int(input())
-graph = [[] for _ in range(N+1)]
-visited = [False] * (N+1)
-lst = [0] * (N+1) # 부모번호 저장 list
-# 1. graph 정보 입력
+graph = [[] * (N+1) for _ in range(N+1)]
+visited = [0] * (N+1)
 for _ in range(N-1):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
-# 2. dfs 호출
-dfs(1, 0)
-# 3. 출력
-print(*lst[2:], sep = "\n")
+dfs(1)
+for i in range(2, N+1):
+    print(visited[i])

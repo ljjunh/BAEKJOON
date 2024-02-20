8e5d84909e1af931103df85ac801676d5d88cc84
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-02-20 16:28:02 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 560 ms, Memory: 38620 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,29 +1,32 @@
 import sys
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(10 ** 6) # 100만번정도 해놓으면 큰 문제 없음
 input = sys.stdin.readline
 
 def dfs(idx):
+    global visited, graph
     visited[idx] = True
-    for i in range(1, n + 1):
+
+    for i in range(1, N + 1):
+        # 방문한적이 없고, 연결되어있으면 dfs(i)호출
         if not visited[i] and graph[idx][i]:
             dfs(i)
+# 0. 입력 및 초기화
+MAX = 1000 + 10 # 문제에서 주어진 정점의 최대값 + 여유분 10개
+N, M = map(int, input().split())
+graph = [[False] * MAX for _ in range(MAX)]
+visited = [False] * MAX
+answer = 0
 
+# 1. graph에 연결 정보 채우기
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u][v] = True
+    graph[v][u] = True
 
-# 0. 입력받기, visited, graph 초기화
-n, m = map(int, input().split()) # n : 노드   m : 엣지
-graph = [[False] * (n + 1) for _ in range(n+1)]
-visited = [False] * (n + 1)
-cnt = 0
-# 1. 그래프 입력
-for i in range(m):
-    x, y = map(int, input().split())
-    graph[x][y] = True
-    graph[y][x] = True
-    
-# 2. dfs호출
-for i in range(1, n + 1):
+# 2. DFS 호출
+for i in range(1, N + 1):
     if not visited[i]:
-        cnt += 1
         dfs(i)
+        answer += 1
 # 3. 출력
-print(cnt)        
+print(answer)

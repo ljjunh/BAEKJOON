40b874a038d18ac5bbede4f9010ca004999508c5
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-03-11 21:19:14 +0900

    [Silver III] Title: N과 M (1), Time: 180 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,20 +1,18 @@
 import sys
 input = sys.stdin.readline
-
-def dfs(n, lst):
+def dfs(n):
     if n == M:
-        ans.append(lst)
+        print(*path)
         return
-    for i in range(1, N + 1):
-        if v[i] == 0:
-            v[i] = 1 
-            dfs(n + 1, lst + [i])
-            v[i] = 0
-
-
+    for i in range(1, N+1):
+        if visited[i]:
+            continue
+        visited[i] = 1
+        path.append(i)
+        dfs(n+1)
+        path.pop()
+        visited[i] = 0
 N, M = map(int, input().split())
-ans = []
-v = [0] * (N + 1)
-dfs(0, [])
-for i in ans:
-    print(*i)
+path = []
+visited = [0] * (N+1)
+dfs(0)

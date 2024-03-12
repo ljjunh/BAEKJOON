af2738ff9f79a8b3dc037d89b66d53978a1fd45f
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-03-12 17:32:04 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(n, s, lst):
+    if n == M:
+        print(*lst)
+        return
+    prev = 0
+    for i in range(s, N):
+        if not visited[i] and prev != arr[i]:
+            prev = arr[i]
+            visited[i] = 1
+            dfs(n+1, i+1, lst+[arr[i]])
+            visited[i] = 0
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+visited = [0] * N
+dfs(0, 0, [])

7b4e0593f3dc9cb57abdcdad53b85afcfaa6ed5b
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-03-12 15:35:15 +0900

    [Silver III] Title: N과 M (5), Time: 212 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,18 +1,16 @@
 def dfs(n, lst):
     if n == M:
-        ans.append(lst)
+        print(*lst)
         return
-    for i in arr:
-        if not visited[i]:
-            visited[i] = 1
-            dfs(n+1, lst + [i])
-            visited[i] = 0
+    for i in range(N):
+        if visited[i]:
+            continue
+        visited[i] = 1
+        dfs(n+1, lst+[arr[i]])
+        visited[i] = 0
 
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort()
-ans = []
-visited = [0] * (arr[-1] + 1)
+visited = [0] * (N+1)
 dfs(0, [])
-for i in ans:
-    print(*i)

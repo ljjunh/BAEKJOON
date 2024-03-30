c880400b5791bed73954e3af6b2bb27bc9c3d2ae
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-03-30 19:00:23 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 196 ms, Memory: 112840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,32 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(n, sm, add, sub, mul, div):
-    global mn, mx
-    # [0] 결과값/중간 값의 범위: int(-1e8)~int(1e8)
-    if sm < int(-1e9) or int(1e9)<sm:
+def recur(n, sm, add, sub, mul, div):
+    global max_num, min_num
+    if n == N:
+        max_num = max(max_num, sm)
+        min_num = min(min_num, sm)
         return
-
-    # [1] 종료조건(정답처리)
-    if n==N:
-        mn = min(mn, sm)
-        mx = max(mx, sm)
-        return
-
-    # [2] 하부호출: 연산자 개수 남았을 경우만 호출 가능
-    if add>0:
-        dfs(n+1, sm+lst[n], add-1, sub, mul, div)
-    if sub>0:
-        dfs(n+1, sm-lst[n], add, sub-1, mul, div)
-    if mul>0:
-        dfs(n+1, sm*lst[n], add, sub, mul-1, div)
-    if div>0:
-        dfs(n+1, int(sm/lst[n]), add, sub, mul, div-1)
+    if add > 0:
+        recur(n+1, sm+arr[n], add-1, sub, mul, div)
+    if sub > 0:
+        recur(n+1, sm-arr[n], add, sub-1, mul, div)
+    if mul > 0:
+        recur(n+1, sm * arr[n], add, sub, mul-1, div)
+    if div > 0:
+        recur(n+1, int(sm/arr[n]), add, sub, mul, div-1)
+    
 
 N = int(input())
-lst = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 add, sub, mul, div = map(int, input().split())
-
-mn, mx = int(1e9), int(-1e9)
-dfs(1, lst[0], add, sub, mul, div)
-print(mx, mn, sep='\n')
+max_num = -2147000000
+min_num = 2147000000
+recur(1, arr[0], add, sub, mul, div)
+print(max_num, min_num, sep="\n")

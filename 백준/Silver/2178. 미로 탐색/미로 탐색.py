8e3237beb4ce18f54178fd4cf3a36b39793f1ddf
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-04-02 23:21:42 +0900

    [Silver I] Title: 미로 탐색, Time: 152 ms, Memory: 114228 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,25 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+from collections import deque
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+def bfs(x, y):
+    visited = [[0] * M for _ in range(N)]
+    visited[x][y] = 1
+    q = deque()
+    q.append((x, y))
+    while q:
+        cx, cy = q.popleft()
+        for k in range(4):
+            nx, ny = cx+dx[k], cy+dy[k]
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and graph[nx][ny]:
+                visited[nx][ny] = visited[cx][cy] + 1
+                q.append((nx, ny))
+    return visited[N-1][M-1]
+
 
 N, M = map(int, input().split())
 graph = [list(map(int, input().rstrip())) for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
-
-q = deque()
-q.append((0, 0))
-visited[0][0] = True
-
-while q:
-    x, y = q.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] and not visited[nx][ny]:
-            graph[nx][ny] = graph[x][y] + 1
-            visited[nx][ny] = True
-            q.append((nx, ny))
-print(graph[N-1][M-1])
+print(bfs(0, 0))

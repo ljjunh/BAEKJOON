c8eff460ff947c681aa74ac48713c3fb14b04ff0
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-04-02 23:35:31 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,35 +1,36 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
+from collections import deque
+
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(x, y):
-    global cnt
-    visited[x][y] = True
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if not visited[nx][ny] and graph[nx][ny]:
-            dfs(nx, ny)
-            cnt += 1
-    return cnt
+def bfs(x, y):
+    global ans
+    cnt = 1
+    visited[x][y] = cnt
+    q = deque()
+    q.append((x, y))
+    while q:
+        cx, cy = q.popleft()
+        for k in range(4):
+            nx, ny = cx + dx[k], cy + dy[k]
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and graph[nx][ny]:
+                cnt += 1
+                visited[nx][ny] = cnt
+                q.append((nx, ny))
+    ans.append(visited[cx][cy])
 
 N = int(input())
-MAX = 25 + 10
-graph = [[False] * MAX for _ in range(MAX)]
-visited = [[False] * MAX for _ in range(MAX)]
-answer = 0
-arr = []
-for i in range(1, N + 1):
-    s = list(map(int, input().rstrip()))
-    for j in range(1, N + 1):
-        graph[i][j] = s[j-1]
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        if not visited[i][j] and graph[i][j]:
-            cnt = 1
-            arr.append(dfs(i, j))
-            answer += 1
-print(answer)
-print(*sorted(arr), sep = "\n")
+graph = [list(map(int, input().rstrip())) for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
+ans = []
+result = 0
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] and not visited[i][j]:
+            bfs(i, j)
+            result += 1
+ans.sort()
+print(result, *ans, sep="\n")

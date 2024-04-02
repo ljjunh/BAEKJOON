2e89212731a0f73fa6e91f20039d57d6fef8639a
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-04-02 23:46:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,30 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-from collections import deque
-
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y):
-    global ans
-    cnt = 1
-    visited[x][y] = cnt
-    q = deque()
-    q.append((x, y))
-    while q:
-        cx, cy = q.popleft()
-        for k in range(4):
-            nx, ny = cx + dx[k], cy + dy[k]
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and graph[nx][ny]:
-                cnt += 1
-                visited[nx][ny] = cnt
-                q.append((nx, ny))
-    ans.append(visited[cx][cy])
+def dfs(x, y):
+    global cnt
+    visited[x][y] = 1
+    for k in range(4):
+        nx, ny = x + dx[k], y + dy[k]
+        if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and graph[nx][ny]:
+            dfs(nx, ny)
+            cnt += 1
+    return cnt
 
 N = int(input())
 graph = [list(map(int, input().rstrip())) for _ in range(N)]
 visited = [[0] * N for _ in range(N)]
 ans = []
 result = 0
+cnt = 0
 for i in range(N):
     for j in range(N):
         if graph[i][j] and not visited[i][j]:
-            bfs(i, j)
+            cnt = 1
+            ans.append(dfs(i,j))
             result += 1
-ans.sort()
-print(result, *ans, sep="\n")
+print(result, *sorted(ans), sep="\n")

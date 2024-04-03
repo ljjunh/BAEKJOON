517d0df044abd91871f75823a5419d5d2dc7e425
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-04-03 22:16:53 +0900

    [Silver II] Title: 촌수계산, Time: 136 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,33 +1,27 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
+from collections import deque
 
-def dfs(idx, count):
-    global graph, visited, answer, end
-    visited[idx] = True
-    if idx == end:
-        answer = count
-        return
-
-    for i in range(1, N+1):
-        if not visited[i] and graph[idx][i]:
-            dfs(i, count + 1)
-
-# 0. 입력 및 초기화
-N = int(input()) 
-start, end = map(int, input().split())
+def bfs(x, e):
+    visited = [0] * (N+1)
+    q = deque()
+    q.append(x)
+    while q:
+        v = q.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = visited[v] + 1
+    if visited[e]:
+        print(visited[e])
+    else:
+        print(-1)
+N = int(input())
+s, e = map(int, input().split())
 M = int(input())
-MAX = 100 + 10
-graph = [[False] * MAX for _ in range(MAX)]
-visited = [False] * MAX
-answer = -1
-# 1. graph 정보 입력
+graph = [[] for _ in range(N+1)]
 for _ in range(M):
-    x, y = map(int, input().split())
-    graph[x][y] = True
-    graph[y][x] = True
-
-# 2. dfs 호출
-dfs(start, 0)
-# 3. 출력
-print(answer)
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+bfs(s, e)
